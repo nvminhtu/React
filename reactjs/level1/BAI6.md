f3d249474111edc3519cd00ba5327b11a0aaa725
Tu Nguyen <nvminhtu@gmail.com>
2017-06-14 23:03:31 +0700

    update nội dung bài 6, xly event trong JSX

---
+++ reactjs/level1/BAI6.md
@@ -0,0 +1,7 @@
+# BÀI 6: State vs Props 
+
+## Cơ bản State và Props
+## Thay đổi state của React Component với setState()
+## Sate và `Single Source of Truth`
+## Render một mảng data (Arry of Data) với map() và JSX
+## Các phần nâng cao và bài đọc khác về State và Props
--- reactjs/source/src/pages/Detail.js
+++ reactjs/source/src/pages/Detail.js
@@ -13,12 +13,28 @@ class Detail extends React.Component {
 			</p>;
 		} 
 	*/
-	render() {
-	    return (<div>
-	        <p>Hello, {chance.first()}.</p>
-	        <p>He/She is from {chance.country({ full: true })}.</p>
-	    </div>);
+
+	/* #note: several elements in JSX
+		render() {
+		    return (<div>
+		        <p>Hello, {chance.first()}.</p>
+		        <p>He/She is from {chance.country({ full: true })}.</p>
+		    </div>);
+		}
+	*/
+
+	// event trong JSX
+	buttonClicked() {
+		console.log('Button was clicked!');
 	}
+
+	render() {
+        return (<div>
+            <p>Hello, {chance.first()}.</p>
+            <p>You're from {chance.country({ full: true })}.</p>
+            <button onClick={this.buttonClicked}>Click me please!</button>
+        </div>);
+    }
 }
 
 export default Detail;
