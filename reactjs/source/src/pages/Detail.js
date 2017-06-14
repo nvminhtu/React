90d443792a9cca83f82965ede8d53b5f0389da6d
Tu Nguyen <nvminhtu@gmail.com>
2017-06-14 22:29:19 +0700

    fix multi element JSX

--- reactjs/source/src/pages/Detail.js
+++ reactjs/source/src/pages/Detail.js
@@ -2,22 +2,22 @@ import React from 'react';
 import Chance from 'chance';
 
 class Detail extends React.Component {
-    render() {
-		/* 
-			//#note: if-else condition
+    /* #note: if else condition
+	    render() {
 			return <p>
 			{
 				chance.first() == 'John' 
 				? 'Hello, John!'
 				: 'Hello, world!'
 			}
-		</p>; */
-		
-		/*  
-			//#note: render several elements at once
-			return <p> Hello, {chance.first()}.</p>
-			<p>You're from {chance.country({full: true})}.</p>;
-		*/
+			</p>;
+		} 
+	*/
+	render() {
+	    return (<div>
+	        <p>Hello, {chance.first()}.</p>
+	        <p>He/She is from {chance.country({ full: true })}.</p>
+	    </div>);
 	}
 }
 
