eab989e64d07bd3766c6b78088f21010572efcf6
Bui Phu <buiphu2901@gmail.com>
2017-06-14 18:44:38 +0700

    insert Detail Component

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DetailComponent from './pages/Detail'
 class Demo extends React.Component {
     render(){
         return (
-            <h1>Hello ReactJS</h1>
+            <div>
+                <h1>Hello ReactJS</h1>
+                <DetailComponent/>
+            </div>
+            
         );
     }
 }
