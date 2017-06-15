7fdd58685097100311b36adff79c3c4fd5ba8b6d
Bui Phu <buiphu2901@gmail.com>
2017-06-16 01:29:26 +0700

    Update using SugerAgent with API github for get user's Repos

--- practice/src/pages/Detail.jsx
+++ practice/src/pages/Detail.jsx
@@ -8,10 +8,12 @@ export default class Detail extends React.Component {
         super(props);
 
         this.state = {
-            data : []
+            data : [],
+            repos : []
         };
         this.handleClick = this.handleClick.bind(this);
         this.handleClickSugerAgent = this.handleClickSugerAgent.bind(this);
+        this.handleClickGithubAPI = this.handleClickGithubAPI.bind(this);
     }
     handleClick(){
         let _this = this;
@@ -22,9 +24,7 @@ export default class Detail extends React.Component {
             })
             .catch(function(error){
                 console.log(error);
-            });
-        
-    
+            });  
     }
     handleClickSugerAgent(){
         let _this = this;
@@ -34,15 +34,40 @@ export default class Detail extends React.Component {
                     _this.setState(_this.state);
                 });
     }
+    // demo using SugerAgent with API github for get user's Repos 
+    handleClickGithubAPI(event){
+        event.preventDefault();
+        let username = this.refs.username.value;
+        let _this = this;
+        request.get(`https://api.github.com/users/${username}/repos`)
+                .then(function(success,failure){
+                    _this.state.repos = success.body;
+                    _this.setState(_this.repos);
+                });
+    }
     render(){
         
         return(
             <div>
+            
             <h1 onClick={this.handleClick} >Click me Axios</h1>
             <h1 onClick={this.handleClickSugerAgent} >Click me SugerAgent</h1>
-                 {
-                    this.state.data.map((item,index)=>(
-                        <p key={index}>Hello {item.name} and  {item.age} year old !!!</p>
+            {
+                this.state.data.map((item,index)=>(
+                    <p key={index}>Hello {item.name} and  {item.age} year old !!!</p>
+                ))             
+            }
+            <hr/>
+            <form>
+                <input type="text" ref="username" />
+                <button onClick={this.handleClickGithubAPI}>Click Me Github API</button>
+            </form>
+                {
+                    this.state.repos.map((item,index)=>(
+                       <div key={index} >
+                            <h3>Name Repo : {item.name} </h3>
+                            <p>Description : {item.description}</p>
+                       </div>
                     ))
                 }
             </div>
