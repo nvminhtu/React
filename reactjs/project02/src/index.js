a6e13614664e4f9b20c3f9277d1828b342da1555
Tu Nguyen <nvminhtu@gmail.com>
2017-06-17 10:46:44 +0700

    clone project 1 -> project 2 ( update more features)

--- reactjs/project02/src/index.js
+++ reactjs/project02/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import OneFeed from './pages/OneFeed';
+import MultiFeeds from './pages/MultiFeeds';
 
 ReactDOM.render(
 	// <OneFeed />,
-	<OneFeed />,
+	<MultiFeeds />,
     document.getElementById('app')
 );
