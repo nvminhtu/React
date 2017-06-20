9d87d402ca38dff8f44acf8a941da500692ff1d3
Tu Nguyen <nvminhtu@gmail.com>
2017-06-20 16:08:42 +0700

    update react router source - step by step ( using BrowserRouter, Match)...

--- reactjs/level1/react-router/src/App.js
+++ reactjs/level1/react-router/src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import { BrowserRouter, Match, Link } from 'react-router'
+import { Home } from './Components'
 
-export default React.createClass({
-  render() {
-    return <div>Hello, React Router!</div>
-  }
-})
+//Using a BrowserRouter and Match 
+//Having a Link to access the path
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Link to="/">Home</Link>
+      <Match exactly pattern="/" component={Home} />
+    </div>
+   </BrowserRouter>
+  , document.getElementById('main'))
