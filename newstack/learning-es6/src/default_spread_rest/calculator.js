53647b76aeaf19a90f256e930ba1c4b800edcbc4
Tu Nguyen <nvminhtu@gmail.com>
2017-08-06 00:09:19 +0700

    upgrade Rest operators

--- newstack/learning-es6/src/default_spread_rest/calculator.js
+++ newstack/learning-es6/src/default_spread_rest/calculator.js
@@ -6,6 +6,22 @@ class Calculator {
   static sum(...input) {
     return input.reduce((sum, value) => sum + value, 0)
   }
+
+  // tn: ehancement for reduce
+  static 
 }
 
 export default Calculator
+
+
+function sumOnlyNumbers() {  
+  var args = arguments;
+  var numbers = filterNumbers();
+  return numbers.reduce((sum, element) => sum + element);
+  function filterNumbers() {
+     return Array.prototype.filter.call(args, 
+       element => typeof element === 'number'
+     );
+  }
+}
+sumOnlyNumbers(1, 'Hello', 5, false); // => 6  
