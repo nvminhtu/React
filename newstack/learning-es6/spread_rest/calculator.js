fa0f94c1f72b7219a720b0eadfe2385f41a5064a
lamanhdai <lanhdai1988@gmail.com>
2017-08-06 17:03:14 +0700

    How to use promise, different promise and ajax

--- newstack/learning-es6/spread_rest/calculator.js
+++ newstack/learning-es6/spread_rest/calculator.js
@@ -7,6 +7,16 @@ class Calculator {
     // reduce function return value from array
     return args.reduce((sum, value) => sum + value, 0)
   }
+
+  // tn: ehancement for reduce
+  static sumUpgraded(...args) {
+    function filterNumbers() {
+      return Array.prototype.filter.call(args, element => typeof element === 'number');
+    }
+    var numbers = fulterNumbers();
+
+    return numbers.reduce((sum, value) => sum + value, 0)
+  }
 }
 
 export default Calculator
