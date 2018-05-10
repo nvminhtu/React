dee05c1a4d0e1e1abdd586d1f2e48cccf05d3adf
Tu Nguyen <nvminhtu@gmail.com>
2018-05-11 01:05:52 +0700

    Test Arrow Function Es6

--- es6/src/index.js
+++ es6/src/index.js
@@ -9,19 +9,87 @@ function component() {
 document.body.appendChild(component()); */
 
 // ------ Coding ES6 here
-function f() {
+/* function f() {
     let x = "data";
     console.log(x);
     
 }
 console.log(f());
+*/
 
-
-// 1-  Let & Const
-let a = 5;
+// 1- Let
+/* let a = 5;
 let b = 100;
 if(true) {
     let a = 60;
     var c = 10;
     console.log(a/c);
 }
+*/
+// 2- Const 
+// fixed, constant value to variable
+
+/* const  a = 50;
+a = 60; // errors right now
+
+const b = "Hello we are here";
+b = "Assign me any task"; // errors two
+
+// if we can use it for array - it will be good
+
+const LANGUAGES = ['JS','JAVA','Python','Ruby','Go'];
+
+LANGUAGES = "JavAScript"; // shows error.
+LANGUAGES.push('JAVA'); // Works fine.
+
+console.log(LANGUAGES); // show data of array
+*/
+
+
+/* 3.Arrow Function */
+
+// Old function
+/* 
+function oldFunc() {
+    console.log('Hello world here');
+}
+
+// New function
+
+var newOne = () => {
+    console.log('Hello baby!!!');
+}
+
+// first part: declaring a variable, next assigning the function to it.
+
+let newOneFunc = (a,b) => {
+    console.log( a + b);
+}
+newOneFunc(30,40);
+
+// Testing function override variable
+ 
+let runFunction = (a = 10, b) => {
+    return a + b;
+}
+runFunction(20); // not working, cause by b not valid
+runFunction(40,10); // 50
+let result = runFunction(40,10);
+console.log(result);
+*/
+
+// 4. For loop
+/* 
+let arr = [4,5,8,9];
+for (let value of arr) {
+    console.log(value);
+}
+
+let string = "Toilatu";
+for (let char of string) {
+    console.log(char);
+}
+*/
+
+
+// 5. Spread Attribute
