cd93351682b7ac509b60cf45778bce881ca31439
Tu Nguyen <nvminhtu@gmail.com>
2018-05-13 16:54:27 +0700

    thực hành lại ghi đè - contructor, super cho nhớ

--- projects/01.es6node/public/vidu1.js
+++ projects/01.es6node/public/vidu1.js
@@ -23,4 +23,19 @@ class Member {
 
 var mem1 = new Member('Tu',29);
 console.log(mem1);
-console.log(mem1.welcomeMember())
+console.log(mem1.welcomeMember());
+
+class Child extends Member {
+    constructor(name, age, hobby) {
+        super(name,age); // kế thừa từ cha
+        this.hobby = hobby;
+    }
+    // override hàm welcomeMember()
+    welcomeMember() {
+        return 'Xin chào mọi người, ' + 'em tên là ' + this.name + ', năm nay em ' + this.age + ' tuổi.' + 'Em thích chơi ' + this.hobby;
+    }
+}
+
+// ke thua
+var member = new Child('Terry',15,'Máy bay');
+console.log(member.welcomeMember());
