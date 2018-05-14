9bfd03d5c17b50653b8b643286705eee593f7781
Tu Nguyen <nvminhtu@gmail.com>
2018-05-14 12:45:54 +0700

    react js khoa pham - basic - 21 videos

---
+++ projects/02.reactnull/public/vidu1.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+var TuNguyen = React.createClass({
+    render: function () {
+        return (
+
+        );
+    }
+});
+
+ReactDOM.render(
+    <h1>ReactJS Khoa Pham</h1>
+    
+
+    ,document.getElementById('root')
+);
---
+++ projects/README.md
@@ -0,0 +1,3 @@
+# TAKE NOTE FOR LEARNING REACT JS
+
+* [1. How to style  Component  - in React JS file](https://codeburst.io/4-four-ways-to-style-react-components-ac6f323da822)
